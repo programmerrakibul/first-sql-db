394074b40b8f8015197993813c4c93a38a91cd8a
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-04-09 13:28:42 +0600

    update operation

--- src/query.sql
+++ src/query.sql
@@ -2,3 +2,11 @@
 INSERT INTO cars 
 (brand, model, year, price, color, condition, sold) VALUES
 ('Toyota', 'Camry', 2018, 25000, 'Blue', 4, false);
+
+-- Updating data
+UPDATE cars SET
+  sold = true
+  WHERE color = 'Blue'
+  AND brand = 'Toyota'
+  AND model = 'Camry';
+
