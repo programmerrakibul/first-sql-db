5b4a0cac105458acb4d12dfb73dc5479dfecaf59
Md. Rakibul Islam <rakibul00206@gmail.com>
2026-04-17 16:45:00 +0600

    populate tables sql added

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,11 @@ import type { TDealership } from "./types/index.js";
   // Create tables and insert data
   const createTables = fs.readFileSync("./src/create-tables.sql", "utf-8");
   const carsData = fs.readFileSync("./src/insert-cars-data.sql", "utf-8");
-  const dealershipsData = fs.readFileSync(
-    "./src/insert-dealerships-data.sql",
-    "utf-8",
-  );
-
   await db.exec(createTables);
   await db.exec(carsData);
-  await db.exec(dealershipsData);
+
+  const populateTables = fs.readFileSync("./src/populate-tables.sql", "utf-8");
+  await db.exec(populateTables);
 
   // Read query
   const query = fs.readFileSync("./src/query.sql", "utf-8");
